3f01b746ec0237956564bc9c68378b628392b31b
Ali Imran <muhammadimran.math@gmail.com>
2025-09-28 21:28:04 +0500

    Drawer setup with toolbar

--- app/src/main/java/com/miassolutions/milkledger/presentation/MainActivity.kt
+++ app/src/main/java/com/miassolutions/milkledger/presentation/MainActivity.kt
@@ -1,17 +1,12 @@
 package com.miassolutions.milkledger.presentation
 
 import android.os.Bundle
-import android.view.Menu
-import android.view.MenuInflater
-import android.view.MenuItem
 import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.MenuProvider
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.lifecycle.Lifecycle
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
@@ -33,7 +28,6 @@ class MainActivity : AppCompatActivity(), ToolbarOwner, BottomNavOwner {
     private lateinit var navController: NavController
     private lateinit var appBarConfiguration: AppBarConfiguration
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -51,7 +45,6 @@ class MainActivity : AppCompatActivity(), ToolbarOwner, BottomNavOwner {
         // Configure top-level destinations (so back button shows hamburger instead of up arrow)
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.settingsFragment,
                 R.id.purchaseFragment,
                 R.id.salesFragment,
                 R.id.statsFragment,
@@ -60,7 +53,8 @@ class MainActivity : AppCompatActivity(), ToolbarOwner, BottomNavOwner {
             binding.drawerLayout
         )
 
-        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.toolbar.setupWithNavController(navController, appBarConfiguration)
+
         // Hook up drawer nav view with nav controller
         binding.navigationView.setupWithNavController(navController)
 
@@ -68,25 +62,7 @@ class MainActivity : AppCompatActivity(), ToolbarOwner, BottomNavOwner {
         // Hook up bottom nav with nav controller
         binding.bottomNavigationView.setupWithNavController(navController)
 
-        // Optional: Toggle for hamburger animation
-        val toggle = ActionBarDrawerToggle(
-            this,
-            binding.drawerLayout,
-            binding.toolbar,
-            R.string.navigation_drawer_open,
-            R.string.navigation_drawer_close
-        )
-        binding.drawerLayout.addDrawerListener(toggle)
-        toggle.syncState()
 
-    }
-
-    // Ensure navController handles Up navigation with drawer support
-    override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(
-            navController,
-            appBarConfiguration
-        ) || super.onSupportNavigateUp()
     }
 
 
