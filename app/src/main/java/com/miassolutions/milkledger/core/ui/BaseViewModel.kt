2c3e7c6516216d61cf4a4c5eb9c11dad1fbbf4e5
Ali Imran <muhammadimran.math@gmail.com>
2025-09-28 15:01:28 +0500

    BaseViewModel defined

---
+++ app/src/main/java/com/miassolutions/milkledger/core/ui/BaseViewModel.kt
@@ -0,0 +1,30 @@
+package com.miassolutions.milkledger.core.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+abstract class BaseViewModel<S : Any>(initialState: S) : ViewModel() {
+
+    private val _uiState = MutableStateFlow(initialState)
+    val uiState: StateFlow<S> = _uiState.asStateFlow()
+
+    private val _uiEvent = MutableSharedFlow<UiEvent>()
+    val uiEvent: SharedFlow<UiEvent> = _uiEvent.asSharedFlow()
+
+    protected fun updateState(reducer: S.() -> S) {
+        _uiState.value = _uiState.value.reducer()
+    }
+
+    protected fun sendEvent(event: UiEvent) {
+        viewModelScope.launch {
+            _uiEvent.emit(event)
+        }
+    }
+}
