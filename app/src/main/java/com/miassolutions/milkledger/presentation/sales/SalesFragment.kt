4790d0629fdc46c72f53d56f34e42519195f79a4
Ali Imran <muhammadimran.math@gmail.com>
2025-10-15 22:18:34 +0500

    Sales Screen Summary linked

--- app/src/main/java/com/miassolutions/milkledger/presentation/sales/SalesFragment.kt
+++ app/src/main/java/com/miassolutions/milkledger/presentation/sales/SalesFragment.kt
@@ -2,14 +2,19 @@ package com.miassolutions.milkledger.presentation.sales
 
 import android.util.Log
 import androidx.fragment.app.viewModels
+import com.google.android.material.datepicker.MaterialDatePicker
 import com.miassolutions.milkledger.R
 import com.miassolutions.milkledger.core.ui.BaseFragment
+import com.miassolutions.milkledger.core.util.toRoundedStr
 import com.miassolutions.milkledger.data.local.entities.CustomerEntity
 import com.miassolutions.milkledger.data.local.entities.SalesEntryEntity
 import com.miassolutions.milkledger.data.local.relations.SaleWithCustomer
 import com.miassolutions.milkledger.databinding.FragmentSalesBinding
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.Instant
 import java.time.LocalDate
+import java.time.ZoneId
+import java.util.Calendar
 import kotlin.math.roundToInt
 
 @AndroidEntryPoint
@@ -25,6 +30,44 @@ class SalesFragment : BaseFragment<FragmentSalesBinding>(FragmentSalesBinding::i
 
 
 
+    }
+
+    override fun setupListeners() {
+        binding.tvSelectedDate.setOnClickListener {
+            val currentDate = viewModel.uiState.value.currentDate
+            val calendar = Calendar.getInstance().apply {
+                set(Calendar.YEAR, currentDate.year)
+                set(Calendar.MONTH, currentDate.monthValue - 1)
+                set(Calendar.DAY_OF_MONTH, currentDate.dayOfMonth)
+            }
+
+            val datePicker = MaterialDatePicker.Builder.datePicker()
+                .setTitleText("Select Date")
+                .setSelection(calendar.timeInMillis)
+                .build()
+
+            datePicker.addOnPositiveButtonClickListener { selectedDateInMillis ->
+                val selectedDate = Instant.ofEpochMilli(selectedDateInMillis)
+                    .atZone(ZoneId.systemDefault())
+                    .toLocalDate()
+
+                viewModel.onDateSelected(selectedDate)
+            }
+
+            datePicker.show(parentFragmentManager, "MaterialDatePicker")
+        }
+
+        binding.btnPrevDate.setOnClickListener {
+            val currentDate = viewModel.uiState.value.currentDate
+            val previousDate = currentDate.minusDays(1)
+            viewModel.onDateSelected(previousDate)
+        }
+
+        binding.btnNextDate.setOnClickListener {
+            val currentDate = viewModel.uiState.value.currentDate
+            val nextDate = currentDate.plusDays(1)
+            viewModel.onDateSelected(nextDate)
+        }
     }
 
     private fun setupSalesRV() {
@@ -56,6 +99,10 @@ class SalesFragment : BaseFragment<FragmentSalesBinding>(FragmentSalesBinding::i
                 tvTotalAmount.text =
                     getString(R.string.rs, state.totalAmount.roundToInt())
 
+                tvDeduction.text = state.totalDeduction.toRoundedStr()
+                tvTotalNetMilk.text = state.totalMilk.toRoundedStr()
+                tvAvgPrice.text = state.avgRatePerLiter.toRoundedStr()
+                tvTotalMilk.text = state.totalMilk.toRoundedStr()
 
             }
         }
