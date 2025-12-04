2627d10d2071925d6a26fe4bf2fc8b571744a055
Muhammad Imran <muhammadimran.math@gmail.com>
2025-12-04 20:45:42 +0500

    sync on

--- app/src/main/java/com/miassolutions/milkledger/presentation/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/miassolutions/milkledger/presentation/dashboard/DashboardFragment.kt
@@ -34,7 +34,7 @@ class DashboardFragment :
 
     override fun setupViews() {
 
-//        syncViewModel.startInitialSync() //todo
+        syncViewModel.startInitialSync() //todo
 
         RemoteConfigHelper.fetchValue(viewLifecycleOwner) { isTrialVersion ->
 
