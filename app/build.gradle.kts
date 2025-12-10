6bff504761bad5359de6f5d09db9b2946d46f05f
Muhammad Imran <muhammadimran.math@gmail.com>
2025-12-10 20:44:29 +0500

    update to ver 1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.miassolutions.milkledger"
         minSdk = 27
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
