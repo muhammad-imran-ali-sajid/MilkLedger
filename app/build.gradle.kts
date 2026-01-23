f1733d50302f7d444a17ad6f5b308e5b94670cac
Muhammad Imran <muhammadimran.math@gmail.com>
2026-01-23 20:06:46 +0500

    color changes in purchase and sale form

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.miassolutions.milkledger"
         minSdk = 27
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.3"
+        versionCode = 4
+        versionName = "2.4"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
