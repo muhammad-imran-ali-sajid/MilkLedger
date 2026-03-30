8faf168803440057b6554f142edaf92056a36cf0
Muhammad Imran <muhammadimran.math@gmail.com>
2026-03-30 09:13:04 +0500

    payment data disabled VERSION 2.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.miassolutions.milkledger"
         minSdk = 27
         targetSdk = 36
-        versionCode = 6
-        versionName = "2.6"
+        versionCode = 7
+        versionName = "2.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
