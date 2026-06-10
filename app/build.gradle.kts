c91dfe0b8694867de5350275bdfb1ee123134fa2
Muhammad Imran <muhammadimran.math@gmail.com>
2026-06-10 21:57:10 +0500

    Fix Crash: update DEPENDENCIES

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,23 +64,41 @@ android {
 }
 
 dependencies {
-
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.legacy.support.v4)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.room.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
     
     implementation("androidx.work:work-runtime-ktx:2.11.2")
     
+    implementation("com.google.dagger:hilt-android:2.57.2")
+    ksp("com.google.dagger:hilt-android-compiler:2.57.2")
+    
     implementation("androidx.hilt:hilt-work:1.3.0")
     ksp("androidx.hilt:hilt-compiler:1.3.0")
+    
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.9.4")
+    
+    implementation("androidx.room:room-runtime:2.8.1")
+    implementation("androidx.room:room-ktx:2.8.1")
+    ksp("androidx.room:room-compiler:2.8.1")
+    
+    implementation("androidx.navigation:navigation-fragment-ktx:2.9.5")
+    implementation("androidx.navigation:navigation-ui-ktx:2.9.5")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
+    
+    implementation("androidx.datastore:datastore-preferences:1.1.7")
+    implementation("androidx.documentfile:documentfile:1.1.0")
+    implementation("androidx.biometric:biometric:1.2.0-alpha05")
     
     implementation("com.google.android.gms:play-services-auth:21.6.0")
     implementation("com.google.api-client:google-api-client-android:2.9.0")
@@ -88,58 +106,37 @@ dependencies {
     implementation("com.google.http-client:google-http-client-gson:2.1.0")
     implementation("com.google.http-client:google-http-client-android:2.1.0")
     
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
-
-    implementation("com.itextpdf:itextg:5.5.10")
-
-
-    // Firebase BOM
     implementation(platform("com.google.firebase:firebase-bom:34.5.0"))
-
-
     implementation("com.google.firebase:firebase-config")
     implementation("com.google.firebase:firebase-analytics")
     implementation("com.google.firebase:firebase-firestore")
     implementation("com.google.firebase:firebase-auth")
-
+    
+    implementation("io.grpc:grpc-api:1.68.1")
+    implementation("io.grpc:grpc-core:1.68.1")
+    implementation("io.grpc:grpc-android:1.68.1")
+    implementation("io.grpc:grpc-okhttp:1.68.1")
+    implementation("io.grpc:grpc-stub:1.68.1")
+    implementation("io.grpc:grpc-protobuf-lite:1.68.1")
+    
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.11.0")
     implementation("com.google.code.gson:gson:2.13.2")
-
-
-
     implementation("org.jetbrains.kotlin:kotlin-reflect")
+    
+    implementation("com.itextpdf:itextg:5.5.10")
+    
+  
+}
 
-    // finger print
-    implementation("androidx.biometric:biometric:1.2.0-alpha05")
-
-
-    // Lifecycle
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.4")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.9.4")
-    // Room Database
-    implementation("androidx.room:room-runtime:2.8.1")
-    implementation("androidx.room:room-ktx:2.8.1")
-    ksp("androidx.room:room-compiler:2.8.1")
-
-
-    // Coroutines
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
-
-    //data store
-    implementation("androidx.datastore:datastore-preferences:1.1.7")
-
-    //hilt
-    implementation("com.google.dagger:hilt-android:2.57.2")
-    ksp("com.google.dagger:hilt-android-compiler:2.57.2")
-
-    // Navigation Component
-    implementation("androidx.navigation:navigation-fragment-ktx:2.9.5")
-    implementation("androidx.navigation:navigation-ui-ktx:2.9.5")
-
-    // fragment for view model code generation
-    implementation("androidx.fragment:fragment-ktx:1.8.9")
-
-    // DocumentFile is a wrapper class in AndroidX that helps you interact with files and folders
-    implementation("androidx.documentfile:documentfile:1.1.0")
-
+configurations.configureEach {
+    resolutionStrategy {
+        force(
+            "io.grpc:grpc-api:1.68.1",
+            "io.grpc:grpc-core:1.68.1",
+            "io.grpc:grpc-android:1.68.1",
+            "io.grpc:grpc-okhttp:1.68.1",
+            "io.grpc:grpc-stub:1.68.1",
+            "io.grpc:grpc-protobuf-lite:1.68.1"
+        )
+    }
 }
