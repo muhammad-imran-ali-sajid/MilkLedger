81b4112da95f76698794e7ac3558f0ae609d8b89
imran <muhammadimran.math@gmail.com>
2026-09-07 16:16:54 +0500

    version updated 4.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.miassolutions.milkledger"
         minSdk = 27
         targetSdk = 36
-        versionCode = 11
-        versionName = "4.2"
+        versionCode = 12
+        versionName = "4.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
